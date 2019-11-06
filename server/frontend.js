ecf80b762676261e7f2a6ec2403cf5bcf6be6fab
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-11-05 19:55:53 -0800

    tinkering with npm scripts

--- server/frontend.js
+++ server/frontend.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// express is not good for production static files, use cdn, or dedicated file server like ngnix, appache:
-app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  });
+}
+
 
 const port = process.env.PORT || 3000;
 
